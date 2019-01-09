5cb78aa582bd1c94160b98d087b06846168993a3
Yentis <yentl178@gmail.com>
2019-01-09 16:35:30 +0100

    add raw link

--- EmoteReplacer.plugin.js
+++ EmoteReplacer.plugin.js
@@ -15,7 +15,7 @@ class EmoteReplacer {
 
     start() {
         if (!global.ZeresPluginLibrary) return window.BdApi.alert("Library Missing",`The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
-        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "LINK_TO_RAW_CODE");
+        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "https://raw.githubusercontent.com/Yentis/betterdiscord-emotereplacer/master/EmoteReplacer.plugin.js");
         fieldChecker = setInterval(checkEmoteNames, 100);
     }
     stop() {
