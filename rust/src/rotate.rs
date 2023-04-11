26e0faa7000dff2fa461a83cbe5adaa4df59d4f2
Yentis <yentl178@gmail.com>
2023-04-11 17:30:11 +0200

    implement spin

--- rust/src/rotate.rs
+++ rust/src/rotate.rs
@@ -2,22 +2,26 @@ use image::{Frame, Rgba};
 use imageproc::geometric_transformations::{self, Interpolation};
 
 pub fn rotate(frames: Vec<Frame>, degrees: f32) -> Vec<Frame> {
-  frames
-    .into_iter()
-    .map(|frame| {
-      let new_buffer = geometric_transformations::rotate_about_center(
+    frames
+        .into_iter()
+        .map(|frame| {
+            rotate_frame(&frame, degrees)
+        })
+        .collect()
+}
+
+pub fn rotate_frame(frame: &Frame, degrees: f32) -> Frame {
+    let new_buffer = geometric_transformations::rotate_about_center(
         frame.buffer(),
         degrees.to_radians(),
         Interpolation::Nearest,
         Rgba([0, 0, 0, 0])
-      );
+    );
 
-      Frame::from_parts(
-        new_buffer,
-        frame.left(),
-        frame.top(),
-        frame.delay()
-      )
-    })
-    .collect()
+    Frame::from_parts(
+      new_buffer,
+      frame.left(),
+      frame.top(),
+      frame.delay()
+    )
 }
--- src/modifiers/spin.ts
+++
@@ -1,3 +0,0 @@
-export function spinGIF (): void {
-  // TODO
-}
